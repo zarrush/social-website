9483099b001b79524c5dac55fdd74405310a4431
zarrush <zarrush9@gmail.com>
2026-08-22 07:51:08 +0330

    Add registration flow and clean up unused URL paths

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,23 +1,13 @@
-from django.urls import path
+from django.urls import path , include
 from . import views
 from django.contrib.auth import views as auth_views
-from account.views import user_login
-from .views import dashboard
+from .views import dashboard, register, user_logout
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # Password change views
-    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-    # Password reset views
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
     path('images/', views.images, name='images'),
     path('people/', views.peoples, name='peoples'),
 
+    path('', include('django.contrib.auth.urls')),
     path('', dashboard, name='dashboard'),
+    path('register/', register, name='register'),
 ]
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,38 +1,22 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm
 
 
-def user_login(request):
+def register(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
-
-        if form.is_valid():
-            cd = form.cleaned_data
-
-            user = authenticate(
-                request,
-                username=cd['username'],
-                password=cd['password']
-            )
-
-            if user is not None:
-                login(request, user)
-                return redirect('dashboard')
-            else:
-                return HttpResponse("Invalid credentials.")
-
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
-        form = LoginForm()
-
-    return render(
-        request,
-        'account/login.html',
-        {'form': form}
-    )
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
 
 
 @login_required
